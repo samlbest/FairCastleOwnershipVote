2b6e2809f5e1cb841cbd7529020c0948019ccb57
Sam Best <sambest86@gmail.com>
2020-05-16 15:16:30 -0500

    Solution structure / skeleton of mod

---
+++ FairCastleOwnershipVoteSubModule.cs
@@ -0,0 +1,24 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace FairCastleOwnershipVote
+{
+    public class FairCastleOwnershipVoteSubModule : MBSubModuleBase
+    {
+        public override void OnCampaignStart(Game game, object starterObject)
+        {
+            if (starterObject is CampaignGameStarter campaignGameStarter)
+            {
+                AddModels(campaignGameStarter);
+            }
+
+            base.OnCampaignStart(game, starterObject);
+        }
+
+        private void AddModels(CampaignGameStarter gameStarter)
+        {
+            gameStarter.AddModel(new FairInventoryCapacityModel());
+        }
+    }
+}
---
+++ FairInventoryCapacityModel.cs
@@ -0,0 +1,17 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents.Party;
+using TaleWorlds.Core;
+using TaleWorlds.Diamond;
+
+namespace FairCastleOwnershipVote
+{
+    public class FairInventoryCapacityModel : DefaultInventoryCapacityModel
+    {
+        public override int CalculateInventoryCapacity(MobileParty mobileParty, StatExplainer explanation = null, int additionalTroops = 0, int additionalSpareMounts = 0, int additionalPackAnimals = 0, bool includeFollowers = false)
+        {
+            InformationManager.DisplayMessage(new InformationMessage("Hello World!"));
+            return base.CalculateInventoryCapacity(mobileParty, explanation, additionalTroops, additionalSpareMounts, additionalPackAnimals, includeFollowers);
+        }
+    }
+}
